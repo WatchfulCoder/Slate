62f6d90118c92accc3e0275cd69fa31fd097ac27
WatchfulCoder <setterblad.mathias@gmail.com>
2026-05-04 15:46:34 +0200

    Initial Commit

---
+++ Slate/SlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlateApp.swift
+//  Slate
+//
+//  Created by Mathias Setterblad on 04/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
